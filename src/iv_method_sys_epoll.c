2f10736bfe29a85c0ea7e249db14ac3f1e4690b6
gorny <gorny@netric.org>
2009-01-02 07:39:15 +0100

    Delete i386 epoll system call definitions.
    Delete the definitions of the three epoll system call numbers for
    i386, as these definitions should be included in any modern distro's
    headers these days.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- src/iv_method_sys_epoll.c
+++ src/iv_method_sys_epoll.c
@@ -27,18 +27,6 @@
 #include "iv_private.h"
 #include "epoll.h"
 
-#if !defined(__NR_epoll_create) && defined(__i386__)
-#include <linux/unistd.h>
-#define __NR_epoll_create	254
-#define __NR_epoll_ctl		255
-#define __NR_epoll_wait		256
-_syscall1(int, epoll_create, int, size)
-_syscall4(int, epoll_ctl, int, epfd, int, op, int, fd,
-	  struct epoll_event *, event)
-_syscall4(int, epoll_wait, int, epfd, struct epoll_event *, pevents,
-	  int, maxevents, int, timeout)
-#endif
-
 #define POLL_BATCH_SIZE		(1024)
 
 
