511dce6417a3de7de9b5c9646c54ff52ae005b02
Lennert Buytenhek <buytenh@wantstofly.org>
2012-06-28 11:16:47 +0200

    iv_getaddrinfo: Fix build warning on OpenBSD 5.1.
    By including netinet/in.h before arpa/inet.h.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- contrib/iv_getaddrinfo/test.c
+++ contrib/iv_getaddrinfo/test.c
@@ -19,11 +19,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <iv.h>
 #include <iv_getaddrinfo.h>
 #include <netdb.h>
-#include <netinet/in.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
