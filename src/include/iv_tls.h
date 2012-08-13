8bfc6a50534432c838e08570c7ad2fd2d8452703
Lennert Buytenhek <buytenh@wantstofly.org>
2012-08-13 15:53:19 +0200

    iv_tls: Switch thread state block offsets from off_t to int.
    So that we don't have to bother trying to find out which header
    file defines off_t.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- src/include/iv_tls.h
+++ src/include/iv_tls.h
@@ -33,7 +33,7 @@ struct iv_tls_user {
 	void		(*deinit_thread)(void *st);
 
 	struct iv_list_head	list;
-	off_t			state_offset;
+	int			state_offset;
 };
 
 void iv_tls_user_register(struct iv_tls_user *);
