658f8333b6b96351e8900e186b2a607a6f81b22f
Lennert Buytenhek <buytenh@wantstofly.org>
2012-08-13 04:51:41 +0200

    test/avl: Turn into a pass/fail test, and run at make check time.
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- test/avl.c
+++ test/avl.c
@@ -1,6 +1,6 @@
 /*
  * ivykis, an event handling library
- * Copyright (C) 2002, 2003 Lennert Buytenhek
+ * Copyright (C) 2002, 2003, 2012 Lennert Buytenhek
  * Dedicated to Marija Kulikova.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -20,51 +20,57 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <iv.h>
 #include <iv_avl.h>
 #include <iv_list.h>
-#include <sys/types.h>
+#include <stdarg.h>
 #include <time.h>
 #include <unistd.h>
 
-static void
-tree_node_print(int depth, struct iv_avl_node *an,
-		void (*print)(struct iv_avl_node *an))
-{
-	int i;
-
-	for (i = 0; i < depth; i++)
-		printf("  ");
-	printf("%p (parent=%p): ", an, an->parent);
-	print(an);
-
-	if (an->left != NULL)
-		tree_node_print(depth + 1, an->left, print);
-	if (an->right != NULL)
-		tree_node_print(depth + 1, an->right, print);
-}
-
-static void
-tree_print(char *msg, struct iv_avl_tree *this,
-	   void (*print)(struct iv_avl_node *an))
-{
-	printf("%s:\n", msg);
-	if (this->root != NULL)
-		tree_node_print(0, this->root, print);
-	printf("\n");
-}
-
-
-struct foo {
+struct node {
 	struct iv_avl_node	an;
 	int			num;
 };
 
-static void printit(struct iv_avl_node *an)
-{
-	struct foo *f = iv_container_of(an, struct foo, an);
+static struct iv_avl_tree x;
 
-	printf("%d (height %d)\n", f->num, f->an.height);
+static void tree_node_print(int depth, struct iv_avl_node *an)
+{
+	struct node *f = iv_container_of(an, struct node, an);
+	int i;
+
+	for (i = 0; i < depth; i++)
+		fprintf(stderr, "  ");
+	fprintf(stderr, "%p (parent=%p): %d (height %d)\n",
+		an, an->parent, f->num, f->an.height);
+
+	if (an->left != NULL)
+		tree_node_print(depth + 1, an->left);
+	if (an->right != NULL)
+		tree_node_print(depth + 1, an->right);
+}
+
+static void tree_print(char *msg)
+{
+	fprintf(stderr, "%s:\n", msg);
+	if (x.root != NULL)
+		tree_node_print(0, x.root);
+	fprintf(stderr, "\n");
+}
+
+static void __attribute__((noreturn)) fatal(const char *fmt, ...)
+{
+	va_list ap;
+	char msg[1024];
+
+	va_start(ap, fmt);
+	vsnprintf(msg, sizeof(msg), fmt, ap);
+	va_end(ap);
+
+	msg[sizeof(msg) - 1] = 0;
+
+	tree_print(msg);
+
+	exit(1);
 }
 
 static int
@@ -77,9 +83,8 @@ tree_node_verify(struct iv_avl_tree *this, struct iv_avl_node *an,
 	int my;
 
 	if (an->parent != parent) {
-		printf("parent mismatch: %p, %p versus %p\n",
-		       an, an->parent, parent);
-		abort();
+		fatal("parent mismatch: %p, %p versus %p",
+		      an, an->parent, parent);
 	}
 
 	(*cnt)++;
@@ -94,7 +99,7 @@ tree_node_verify(struct iv_avl_tree *this, struct iv_avl_node *an,
 			err++;
 
 		if (err)
-			printf("violated %p < %p < %p\n", min, an, max);
+			fatal("violated %p < %p < %p", min, an, max);
 	}
 
 	hl = 0;
@@ -106,13 +111,11 @@ tree_node_verify(struct iv_avl_tree *this, struct iv_avl_node *an,
 		hr = tree_node_verify(this, an->right, an, an, max, cnt);
 
 	if (abs(hl - hr) > 1)
-		printf("balance mismatch!\n");
+		fatal("balance mismatch: %d vs %d", hl, hr);
 
 	my = 1 + ((hl > hr) ? hl : hr);
-	if (an->height != my) {
-		printf("height mismatch: %d vs %d/%d\n", an->height, hl, hr);
-		abort();
-	}
+	if (an->height != my)
+		fatal("height mismatch: %d vs %d/%d", an->height, hl, hr);
 
 	return my;
 }
@@ -125,17 +128,19 @@ static void tree_check(struct iv_avl_tree *this, int expected_count)
 	if (this->root != NULL)
 		tree_node_verify(this, this->root, NULL, NULL, NULL, &count);
 
-	if (expected_count >= 0 && expected_count != count) {
-		printf("count mismatch: %d versus %d\n", count, expected_count);
-		abort();
-	}
+	if (expected_count >= 0 && expected_count != count)
+		fatal("count mismatch: %d versus %d", count, expected_count);
 }
 
 
+#define NUM	16384
+
+static struct node *f[NUM];
+
 static int docomp(struct iv_avl_node *_a, struct iv_avl_node *_b)
 {
-	struct foo *a = iv_container_of(_a, struct foo, an);
-	struct foo *b = iv_container_of(_b, struct foo, an);
+	struct node *a = iv_container_of(_a, struct node, an);
+	struct node *b = iv_container_of(_b, struct node, an);
 
 	if (a->num < b->num)
 		return -1;
@@ -143,13 +148,6 @@ static int docomp(struct iv_avl_node *_a, struct iv_avl_node *_b)
 		return 1;
 	return 0;
 }
-
-
-
-#define NUM	16384
-
-static struct iv_avl_tree x;
-static struct foo *f[NUM];
 
 static int mkrand(void)
 {
@@ -165,51 +163,36 @@ static int mkrand(void)
 
 int main()
 {
-	struct iv_avl_node *an;
 	int i;
+
+	alarm(300);
 
 	srand(time(NULL) ^ getpid());
 
 	INIT_IV_AVL_TREE(&x, docomp);
 
+	tree_check(&x, 0);
+
 	for (i = 0; i < NUM; i++) {
 		int ret;
 
-		if ((i & 1023) == 0)
-			printf("inserting #%d\n", i);
-
-		f[i] = malloc(sizeof(struct foo));
+		f[i] = malloc(sizeof(struct node));
 
 		do {
 			f[i]->num = mkrand();
 			ret = iv_avl_tree_insert(&x, &f[i]->an);
 		} while (ret < 0);
 
-		if (ret) {
-			fprintf(stderr, "error %d\n", ret);
-			exit(1);
-		}
-
 		tree_check(&x, i + 1);
 	}
 
-	tree_check(&x, NUM);
-
-	iv_avl_tree_for_each (an, &x)
-		printit(an);
-
 	for (i = 0; i < NUM; i++) {
-		if ((i & 1023) == 0)
-			printf("deleting #%d\n", i);
-
-		an = &f[i]->an;
-
-		iv_avl_tree_delete(&x, an);
+		iv_avl_tree_delete(&x, &f[i]->an);
 
 		tree_check(&x, NUM - i - 1);
-	}
 
-	tree_print("deletions done", &x, printit);
+		free(f[i]);
+	}
 
 	return 0;
 }
