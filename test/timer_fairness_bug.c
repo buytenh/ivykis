buytenh/ivykis

commit 7190532aca902865ec8e578ac1c23d504f52a75a

iv_timer: Implement fairness for timers.
Split the dequeueing of expired timers and the running of their
handlers into two separate phases, to prevent the situation where
timer handlers registering timers in the past causes execution of
tasks and file descriptor or handle handlers to be starved.

Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_timer.c
@@ -70,17 +70,37 @@ const struct timespec *iv_get_soonest_timeout(const struct iv_state *st)
 
 void iv_run_timers(struct iv_state *st)
 {
+	struct iv_list_head timers;
+
+	if (!st->num_timers)
+		return;
+
+	INIT_IV_LIST_HEAD(&timers);
+
+	if (!st->time_valid) {
+		st->time_valid = 1;
+		iv_time_get(&st->time);
+	}
+
 	while (st->num_timers) {
 		struct iv_timer_ *t = st->ratnode.first_leaf.child[1];
-
-		if (!st->time_valid) {
-			st->time_valid = 1;
-			iv_time_get(&st->time);
-		}
 
 		if (timespec_gt(&t->expires, &st->time))
 			break;
 		iv_timer_unregister((struct iv_timer *)t);
+
+		iv_list_add_tail(&t->list_expired, &timers);
+		t->index = 0;
+	}
+
+	while (!iv_list_empty(&timers)) {
+		struct iv_timer_ *t;
+
+		t = iv_list_entry(timers.next, struct iv_timer_, list_expired);
+
+		iv_list_del(&t->list_expired);
+		t->index = -1;
+
 		t->handler(t->cookie);
 	}
 }
@@ -278,35 +298,41 @@ void iv_timer_unregister(struct iv_timer *_t)
 			 "on the heap");
 	}
 
-	if (t->index > st->num_timers) {
-		iv_fatal("iv_timer_unregister: timer index %d > %d",
-			 t->index, st->num_timers);
-	}
+	if (t->index) {
+		if (t->index > st->num_timers) {
+			iv_fatal("iv_timer_unregister: timer index %d > %d",
+				 t->index, st->num_timers);
+		}
 
-	p = iv_timer_get_node(st, t->index);
-	if (*p != t) {
-		iv_fatal("iv_timer_unregister: unregistered timer "
-			 "index belonging to other timer");
-	}
+		p = iv_timer_get_node(st, t->index);
+		if (*p != t) {
+			iv_fatal("iv_timer_unregister: unregistered timer "
+				 "index belonging to other timer");
+		}
 
-	st->numobjs--;
+		m = iv_timer_get_node(st, st->num_timers);
+		*p = *m;
+		(*p)->index = t->index;
+		*m = NULL;
 
-	m = iv_timer_get_node(st, st->num_timers);
-	*p = *m;
-	(*p)->index = t->index;
-	*m = NULL;
+		if (st->rat_depth > 0 &&
+		    st->num_timers == (1 << (st->rat_depth *
+					     IV_TIMER_SPLIT_BITS))) {
+			iv_timer_radix_tree_remove_level(st);
+		}
+		st->num_timers--;
 
-	if (st->rat_depth > 0 &&
-	    st->num_timers == (1 << (st->rat_depth * IV_TIMER_SPLIT_BITS)))
-		iv_timer_radix_tree_remove_level(st);
-	st->num_timers--;
-
-	if (p != m) {
-		pull_up(st, (*p)->index, p);
-		push_down(st, (*p)->index, p);
+		if (p != m) {
+			pull_up(st, (*p)->index, p);
+			push_down(st, (*p)->index, p);
+		}
+	} else {
+		iv_list_del(&t->list_expired);
 	}
 
 	t->index = -1;
+
+	st->numobjs--;
 }
 
 int iv_timer_registered(const struct iv_timer *_t)

src/iv_private.h
@@ -67,6 +67,7 @@ struct iv_timer_ {
 	/*
 	 * Private data.
 	 */
+	struct iv_list_head	list_expired;
 	int			index;
 };
 

commit 341ddb96351c4a87c16b8fdc536a31617c69b654

Fix object reference counting bug when unregistering an expired timer.
If two timers expire during the same poll epoch, and the timer whose
handler happens to run first unregisters the other timer, we would
accidentally decrement the count of objects registered to the current
thread's ivykis event loop, st->numobjs, one too many times.

This could cause the object count to drop to zero, and iv_main() to
return while there are still objects registered, or it could cause the
object count to go negative, and iv_main() to not return even though
it should.

This patch fixes the logic in iv_timer_unregister() to only decrement
st->numobjs if the timer we are unregistering was on the timer heap,
and to avoid decrementing if the timer is on the list of expired
timers, and that fixes this bug.  And while we're at it, let's add a
test case to catch this bug.

Fixes: 7190532aca90 ("iv_timer: Implement fairness for timers.")
Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_timer.c
@@ -341,13 +341,13 @@ void iv_timer_unregister(struct iv_timer *_t)
 			pull_up(st, (*p)->index, p);
 			push_down(st, (*p)->index, p);
 		}
+
+		st->numobjs--;
 	} else {
 		iv_list_del(&t->list_expired);
 	}
 
 	t->index = -1;
-
-	st->numobjs--;
 }
 
 int iv_timer_registered(const struct iv_timer *_t)

test/Makefile.am
@@ -11,6 +11,7 @@ TESTS			= avl				\
 			  struct_sizes			\
 			  timer				\
 			  timer_fairness		\
+			  timer_fairness_bug		\
 			  timer_order			\
 			  timer_past			\
 			  timer_zero

test/timer_fairness_bug.c
@@ -1,0 +1,63 @@
+/*
+ * ivykis, an event handling library
+ * Copyright (C) 2017 Lennert Buytenhek
+ * Dedicated to Marija Kulikova.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version
+ * 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 2.1 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License version 2.1 along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iv.h>
+
+static struct iv_timer timer1;
+static struct iv_timer timer2;
+
+static void handler1(void *_t)
+{
+	if (iv_timer_registered(&timer2))
+		iv_timer_unregister(&timer2);
+}
+
+static void handler2(void *_t)
+{
+	if (iv_timer_registered(&timer1))
+		iv_timer_unregister(&timer1);
+}
+
+int main()
+{
+	alarm(5);
+
+	iv_init();
+
+	iv_validate_now();
+
+	IV_TIMER_INIT(&timer1);
+	timer1.expires = iv_now;
+	timer1.handler = handler1;
+	iv_timer_register(&timer1);
+
+	IV_TIMER_INIT(&timer2);
+	timer2.expires = iv_now;
+	timer2.handler = handler2;
+	iv_timer_register(&timer2);
+
+	iv_main();
+
+	iv_deinit();
+
+	return 0;
+}