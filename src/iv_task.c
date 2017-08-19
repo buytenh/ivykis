buytenh/ivykis

commit 30a281489a5bb43716d8a843c8eddacbe641b8ea

iv_task: Allow task registered by another task to run immediately.
If the handler for iv_task X registers another iv_task Y that has
not yet run in this epoch, run the handler for Y within this epoch
instead of letting iv_task's starvation prevention logic defer the
execution of Y's handler to the next epoch.

Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_task.c
@@ -30,8 +30,12 @@ void iv_task_init(struct iv_state *st)
 void iv_run_tasks(struct iv_state *st)
 {
 	struct iv_list_head tasks;
+	uint32_t epoch;
 
 	__iv_list_steal_elements(&st->tasks, &tasks);
+	epoch = ++st->task_epoch;
+
+	st->tasks_current = &tasks;
 	while (!iv_list_empty(&tasks)) {
 		struct iv_task_ *t;
 
@@ -40,15 +44,19 @@ void iv_run_tasks(struct iv_state *st)
 
 		st->numobjs--;
 
+		t->epoch = epoch;
 		t->handler(t->cookie);
 	}
+	st->tasks_current = NULL;
 }
 
 void IV_TASK_INIT(struct iv_task *_t)
 {
+	struct iv_state *st = iv_get_state();
 	struct iv_task_ *t = (struct iv_task_ *)_t;
 
 	INIT_IV_LIST_HEAD(&t->list);
+	t->epoch = st->task_epoch;
 }
 
 void iv_task_register(struct iv_task *_t)
@@ -60,7 +68,10 @@ void iv_task_register(struct iv_task *_t)
 		iv_fatal("iv_task_register: called with task still on a list");
 
 	st->numobjs++;
-	iv_list_add_tail(&t->list, &st->tasks);
+	if (st->tasks_current == NULL || t->epoch == st->task_epoch)
+		iv_list_add_tail(&t->list, &st->tasks);
+	else
+		iv_list_add_tail(&t->list, st->tasks_current);
 }
 
 void iv_task_unregister(struct iv_task *_t)

src/iv_private.h
@@ -54,6 +54,7 @@ struct iv_task_ {
 	 * Private data.
 	 */
 	struct iv_list_head	list;
+	uint32_t		epoch;
 };
 
 struct iv_timer_ {

src/iv_private_posix.h
@@ -37,6 +37,8 @@ struct iv_state {
 
 	/* iv_task.c  */
 	struct iv_list_head	tasks;
+	struct iv_list_head	*tasks_current;
+	uint32_t		task_epoch;
 
 	/* iv_timer.c  */
 	struct timespec		time;

src/iv_private_win32.h
@@ -34,6 +34,8 @@ struct iv_state {
 
 	/* iv_task.c  */
 	struct iv_list_head	tasks;
+	struct iv_list_head	*tasks_current;
+	uint32_t		task_epoch;
 
 	/* iv_timer.c  */
 	struct timespec		time;

commit 003b61c16f0aeda03c2f2ada25a74c8fac3d1d0f

Fix segfault when calling IV_TASK_INIT() before iv_init().
We don't guarantee that IV_TASK_INIT() can safely be called before
iv_init(), and the iv_init.3 man page actually says the following:

	Each thread that wants to use ivykis must call iv_init before
	any other ivykis functions are called.

Regardless, calling IV_TASK_INIT() before iv_init() used to work okay
before commit 30a281489a5b ("iv_task: Allow task registered by another
task to run immediately."), and, depending on how you look at it, this
commit could be considered to have caused a regression.

This patch fixes IV_TASK_INIT() so that it won't crash if it is called
before iv_init(), by using a task epoch value of zero in case we are
called from a thread that has no attached ivykis event loop.

Fixes: 30a281489a5b ("iv_task: Allow task registered by another task to run immediately.")
Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_task.c
@@ -56,7 +56,7 @@ void IV_TASK_INIT(struct iv_task *_t)
 	struct iv_task_ *t = (struct iv_task_ *)_t;
 
 	INIT_IV_LIST_HEAD(&t->list);
-	t->epoch = st->task_epoch;
+	t->epoch = (st != NULL) ? st->task_epoch : 0;
 }
 
 void iv_task_register(struct iv_task *_t)