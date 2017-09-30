buytenh/ivykis

commit 78c0a307e049b75c4909a55c06334df6531d456c

iv_event: Optimize away creation of iv_event_raw in single-threaded apps.
Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_event.c
@@ -121,7 +121,7 @@ int iv_event_register(struct iv_event *this)
 	struct iv_event_thr_info *tinfo =
 		__iv_tls_user_ptr(st, &iv_event_tls_user);
 
-	if (!tinfo->event_count++) {
+	if (!tinfo->event_count++ && is_mt_app()) {
 		if (!iv_event_use_event_raw) {
 			if (event_rx_on(st) == 0)
 				tinfo->u.st = st;
@@ -157,7 +157,7 @@ void iv_event_unregister(struct iv_event *this)
 		mutex_unlock(&tinfo->list_mutex);
 	}
 
-	if (!--tinfo->event_count) {
+	if (!--tinfo->event_count && is_mt_app()) {
 		if (!iv_event_use_event_raw) {
 			event_rx_off(tinfo->u.st);
 			tinfo->u.st = NULL;

src/iv_private_posix.h
@@ -165,6 +165,11 @@ struct iv_fd_poll_method {
 
 extern pthr_key_t iv_state_key;
 
+static inline int is_mt_app(void)
+{
+	return pthreads_available();
+}
+
 static inline struct iv_state *iv_get_state(void)
 {
 	return pthr_getspecific(&iv_state_key);

src/iv_private_win32.h
@@ -74,6 +74,11 @@ void iv_time_init(struct iv_state *st);
 
 extern DWORD iv_state_index;
 
+static inline int is_mt_app(void)
+{
+	return 1;
+}
+
 static inline struct iv_state *iv_get_state(void)
 {
 	return TlsGetValue(iv_state_index);

commit 0b652b44de96dad66e941130241df87b595605d3

iv_event: Hold a reference on the main loop for each registered iv_event.
Since commit 78c0a307e049 ("iv_event: Optimize away creation of
iv_event_raw in single-threaded apps."), registering an iv_event object
in a single-threaded application does not cause the reference count for
the ivykis main loop to be bumped anymore, which will make iv_main()
return even when there are iv_event objects registered.  Fix this by
bumping the reference count on the main loop by hand for each iv_event
that is registered.

Fixes: 78c0a307e049 ("iv_event: Optimize away creation of iv_event_raw in single-threaded apps.")
Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_event.c
@@ -97,6 +97,8 @@ int iv_event_register(struct iv_event *this)
 {
 	struct iv_state *st = iv_get_state();
 
+	st->numobjs++;
+
 	if (!st->event_count++ && is_mt_app()) {
 		if (!iv_event_use_event_raw && event_rx_on(st))
 			iv_event_use_event_raw = 1;
@@ -136,6 +138,8 @@ void iv_event_unregister(struct iv_event *this)
 			event_rx_off(st);
 		}
 	}
+
+	st->numobjs--;
 }
 
 void iv_event_post(struct iv_event *this)