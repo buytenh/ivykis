buytenh/ivykis

commit 09857826675c079fa3c02ae78a7db2b5d5b42c83

iv_event: Optimize out one mutex lock/unlock pair per event delivery iteration.
Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_event.c
@@ -48,19 +48,28 @@ static void __iv_event_run_pending_events(void *_tinfo)
 
 	mutex_lock(&tinfo->list_mutex);
 
+	if (iv_list_empty(&tinfo->pending_events)) {
+		mutex_unlock(&tinfo->list_mutex);
+		return;
+	}
+
 	__iv_list_steal_elements(&tinfo->pending_events, &events);
-	while (!iv_list_empty(&events)) {
+	while (1) {
 		struct iv_event *ie;
+		int empty_now;
 
 		ie = iv_container_of(events.next, struct iv_event, list);
 		iv_list_del_init(&ie->list);
+		empty_now = !!iv_list_empty(&events);
 
 		mutex_unlock(&tinfo->list_mutex);
+
 		ie->handler(ie->cookie);
+		if (empty_now)
+			break;
+
 		mutex_lock(&tinfo->list_mutex);
 	}
-
-	mutex_unlock(&tinfo->list_mutex);
 }
 
 static void iv_event_tls_init_thread(void *_tinfo)

commit efafd783aa834e90aba5ad4b7f277a8471586e62

iv_event: Fix crash in __iv_event_run_pending_events().
Commit 09857826675c ("iv_event: Optimize out one mutex lock/unlock pair
per event delivery iteration.") tried to optimize out a lock by caching
the `iv_list_empty` operation.  It is possible to remove item from the
list while `ie->handler(ie->cookie)` runs, which could invalidate the
value of the cached `empty_now` variable.  If no item left on the list,
it should stop the loop, but it cached continue causing undefined behavior.

Reported-by: Antal Nemes <antal.nemes@balabit.com>
Fixes: 09857826675c ("iv_event: Optimize out one mutex lock/unlock pair per event delivery iteration.")
Signed-off-by: kokan <peter.kokai@balabit.com>
Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

src/iv_event.c
@@ -58,6 +58,11 @@ static void __iv_event_run_pending_events(void *_st)
 			break;
 
 		___mutex_lock(&st->event_list_mutex);
+
+		if (iv_list_empty(&events)) {
+			___mutex_unlock(&st->event_list_mutex);
+			break;
+		}
 	}
 }
 